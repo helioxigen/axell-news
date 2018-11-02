523bfa0eec7b54525e6c7b23cbcaa0f36026df7b
rolling <galixig3n@gmail.com>
2018-11-03 01:00:37 +0200

    api

---
+++ src/api/index.ts
@@ -0,0 +1,19 @@
+import Axios from "axios";
+import { TopHeadlinesResponse } from "./types";
+
+const { API_KEY, API_ENDPOINT } = require("../../app.config.json");
+
+const inst = Axios.create({
+    baseURL: API_ENDPOINT,
+    params: {
+        apiKey: API_KEY
+    }
+});
+
+export const getNews = (country?: string, category?: string) =>
+    inst.get<TopHeadlinesResponse>("top-headlines", {
+        params: {
+            country,
+            category
+        }
+    });
---
+++ src/api/types.ts
@@ -0,0 +1,19 @@
+type Article = {
+    source: {
+        id?: string;
+        name: string;
+    };
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+};
+
+export type TopHeadlinesResponse = {
+    status: string | "ok";
+    totalResults: number;
+    articles: Article[];
+};
