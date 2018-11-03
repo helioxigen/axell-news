d835d7b8664ee8cf84ea5e672c4cec4df70ba3f9
rolling <galixig3n@gmail.com>
2018-11-03 02:24:05 +0200

    Heading line

--- src/components/atoms/PageHeading.tsx
+++ src/components/atoms/PageHeading.tsx
@@ -2,5 +2,5 @@ import styled from "styled-components";
 import * as React from "react";
 
 export default ({ children }) => (
-    <h1 className="uk-heading-line uk-text-center uk-h1">{children}</h1>
+    <h1 className="uk-heading-line uk-text-center uk-h2"><span>{children}</span></h1>
 );
